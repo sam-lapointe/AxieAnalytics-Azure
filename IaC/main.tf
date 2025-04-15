a0753d00decebd0519bab908f68b25c8b13603ec
sam-lapointe <samuellapointebusiness@gmail.com>
2025-04-15 14:42:56 -0400

    Fixed Key Vault Deletion Issue and updated pipeline.

--- IaC/main.tf
+++ IaC/main.tf
@@ -12,7 +12,11 @@ terraform {
 
 provider "azurerm" {
   resource_provider_registrations = "none"
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+  }
 
   subscription_id = var.subscription_id
 }
