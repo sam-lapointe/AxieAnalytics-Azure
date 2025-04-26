356fe279c45d7fa87c314aae6482c70f57dc1e13
sam-lapointe <samuellapointebusiness@gmail.com>
2025-04-26 13:15:07 -0400

    Test Removed Function App Staging Slot.

--- IaC/modules/function-app/main.tf
+++ IaC/modules/function-app/main.tf
@@ -97,48 +97,48 @@ resource "azurerm_linux_function_app" "function_app" {
 #   }
 # }
 
-resource "azurerm_linux_function_app_slot" "function_app_staging_slot" {
-  name                       = "staging"
-  tags                       = local.tags_func
+# resource "azurerm_linux_function_app_slot" "function_app_staging_slot" {
+#   name                       = "staging"
+#   tags                       = local.tags_func
 
-  function_app_id                 = azurerm_linux_function_app.function_app.id
-  storage_account_name            = var.storage_account_name
-  storage_account_access_key      = var.storage_account_access_key
-  key_vault_reference_identity_id = var.umi_key_vault
+#   function_app_id                 = azurerm_linux_function_app.function_app.id
+#   storage_account_name            = var.storage_account_name
+#   storage_account_access_key      = var.storage_account_access_key
+#   key_vault_reference_identity_id = var.umi_key_vault
 
-  site_config {
-    application_stack {
-      python_version = var.python_version
-    }
+#   site_config {
+#     application_stack {
+#       python_version = var.python_version
+#     }
 
-    application_insights_connection_string = azurerm_application_insights.app_insights.connection_string
-    application_insights_key               = azurerm_application_insights.app_insights.instrumentation_key
+#     application_insights_connection_string = azurerm_application_insights.app_insights.connection_string
+#     application_insights_key               = azurerm_application_insights.app_insights.instrumentation_key
 
-    dynamic "ip_restriction" {
-      for_each = var.authorized_ips
-      content {
-        ip_address = "${ip_restriction.value}/32"
-        action     = "Allow"
-      }
-    }
-  }
+#     dynamic "ip_restriction" {
+#       for_each = var.authorized_ips
+#       content {
+#         ip_address = "${ip_restriction.value}/32"
+#         action     = "Allow"
+#       }
+#     }
+#   }
 
-  app_settings = var.app_settings
+#   app_settings = var.app_settings
 
-  identity {
-    type = "UserAssigned"
-    identity_ids = var.user_managed_identities
-  }
+#   identity {
+#     type = "UserAssigned"
+#     identity_ids = var.user_managed_identities
+#   }
 
-  lifecycle {
-    ignore_changes = [
-      tags["hidden-link: /app-insights-conn-string"],
-      tags["hidden-link: /app-insights-instrumentation-key"],
-      tags["hidden-link: /app-insights-resource-id"]
-    ]
-    # create_before_destroy = false
-    # replace_triggered_by = [
-    #   null_resource.trigger_modification
-    # ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [
+#       tags["hidden-link: /app-insights-conn-string"],
+#       tags["hidden-link: /app-insights-instrumentation-key"],
+#       tags["hidden-link: /app-insights-resource-id"]
+#     ]
+#     create_before_destroy = false
+#     replace_triggered_by = [
+#       null_resource.trigger_modification
+#     ]
+#   }
+# }
