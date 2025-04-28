a34d19d7d7f4e4ef8fcb40d78704855f6556d54d
sam-lapointe <samuellapointebusiness@gmail.com>
2025-04-28 19:53:37 -0400

    Added outputs to IaC.

---
+++ IaC/outputs.tf
@@ -0,0 +1,7 @@
+output "webhook_function_hostname" {
+    value = module.webhook_function_app.function_app_hostname
+}
+
+output "database_hostname" {
+    value = module.postgresql_server.hostname
+}
