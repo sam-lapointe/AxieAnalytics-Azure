3c5233269e374059068a6b26374849328fe31454
sam-lapointe <samuellapointebusiness@gmail.com>
2025-05-05 10:57:20 -0400

    Fix Terraform Output.

--- IaC/outputs.tf
+++ IaC/outputs.tf
@@ -3,7 +3,7 @@ output "webhook_function_hostname" {
 }
 
 output "webhook_function_name" {
-  value = module.webhook_function_app.name
+  value = module.webhook_function_app.function_app_name
 }
 
 output "sales_function_hostname" {
