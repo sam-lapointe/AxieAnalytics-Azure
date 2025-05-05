32a2d49d107c0519533196d2ec9ae3dffbd8db1b
sam-lapointe <samuellapointebusiness@gmail.com>
2025-05-05 09:48:03 -0400

    Updated CI/CD for store_sales service.

--- IaC/modules/function-app/outputs.tf
+++ IaC/modules/function-app/outputs.tf
@@ -1,3 +1,7 @@
 output "function_app_hostname" {
   value = azurerm_linux_function_app.function_app.default_hostname
 }
+
+output "function_app_name" {
+  value = azurerm_linux_function_app.function_app.name
+}
--- IaC/outputs.tf
+++ IaC/outputs.tf
@@ -2,6 +2,18 @@ output "webhook_function_hostname" {
   value = module.webhook_function_app.function_app_hostname
 }
 
+output "webhook_function_name" {
+  value = module.webhook_function_app.name
+}
+
+output "sales_function_hostname" {
+  value = module.store_sales_function_app.function_app_hostname
+}
+
+output "sales_function_name" {
+  value = module.store_sales_function_app.function_app_name
+}
+
 output "database_hostname" {
   value = module.postgresql_server.hostname
 }
