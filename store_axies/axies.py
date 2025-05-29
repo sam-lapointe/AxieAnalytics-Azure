514cd5c5d347cb3d56939fe3861e5cdbb785e7ad
sam-lapointe <samuellapointebusiness@gmail.com>
2025-05-29 15:16:59 -0400

    Started store_axies.

--- store_axies/axies.py
+++ store_axies/axies.py
@@ -25,4 +25,14 @@ class Axie:
         self.__sale_date = sale_date
 
     async def get_axie_data(self) -> dict:
-        pass
+        api_url = "https://api-gateway.skymavis.com/graphql/axie-marketplace"
+        headers = {
+            "accept": "application/json, multipart/mixed",
+            "content-type": "application/json",
+            "x-api-key": self.__api_key
+        }
+        body = f"""{{
+            "query":"query MyQuery($lastNDays: Int = 30) {{\\n  axie(axieId: \\"{self.__axie_id}\\") {{\\n    earnedAxpStat(lastNDays: $lastNDays)\\n    bodyShape\\n    breedCount\\n    class\\n    title\\n    parts {{\\n      id\\n    }}\\n    image\\n    axpInfo {{\\n      level\\n      xp\\n    }}\\n  }}\\n}}",
+            "operationName":"MyQuery",
+            "variables":{{}}
+        }}"""
