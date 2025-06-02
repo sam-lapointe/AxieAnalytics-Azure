34b482bc7750b76db34f2be24b8d87267ffdf359
sam-lapointe <samuellapointebusiness@gmail.com>
2025-06-02 15:02:22 -0400

    Added missing Terraform outputs for store_axies.

--- IaC/outputs.tf
+++ IaC/outputs.tf
@@ -14,6 +14,14 @@ output "sales_function_name" {
   value = module.store_sales_function_app.function_app_name
 }
 
+output "axies_function_hostname" {
+  value = modules.store_axies_function_app.function_app_hostname
+}
+
+output "axies_function_name" {
+  value = modules.store_axies_function_app.function_app_name
+}
+
 output "database_hostname" {
   value = module.postgresql_server.hostname
 }
