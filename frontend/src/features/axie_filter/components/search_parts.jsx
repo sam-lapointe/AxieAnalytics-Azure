bbd976e8129ebfb0f2ff56addf855548a5bd1fe5
sam-lapointe <samuellapointebusiness@gmail.com>
2025-06-10 14:43:28 -0400

    Started search_parts component.

--- frontend/src/features/axie_filter/components/search_parts.jsx
+++ frontend/src/features/axie_filter/components/search_parts.jsx
@@ -1,13 +1,45 @@
 import "react"
+import { useState, useRef } from "react"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import {
+  Popover,
+  PopoverContent,
+  PopoverAnchor,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 
 
 export function SearchParts() {
+    const [parts, setParts] = useState({})
+    const [inputValue, setInputValue] = useState("")
+    const [isInputFocus, setIsInputFocus] = useState(false)
+
     return (
         <div>
             <h3 className="text-lg font-medium">Parts</h3>
             
-            <div className="flex gap-3 items-center">
+            <div className="">
+                <Popover open={isInputFocus}>
+                    <PopoverAnchor asChild>
+                        <Input 
+                            placeholder="Search parts..."
+                            onFocus={() => setIsInputFocus(true)}
+                            onBlur={() => setIsInputFocus(false)}
+                            onChange={(e) => {setInputValue(e.target.value)}}
+                            className = "focus-visible:ring-0 focus-visible:ring-offset-0 focus:outline-none"
+                        />
+                    </PopoverAnchor>
+                    <PopoverContent
+                        onOpenAutoFocus={(e) => e.preventDefault()}
+                        className="w-full min-w-[var(--radix-popover-trigger-width)]"
+                    >
+                        <div>
+                            <p>Hello</p>
+                            <p>Hello</p>
+                        </div>
+                    </PopoverContent>
+                </Popover>
             </div>
         </div>
     )
