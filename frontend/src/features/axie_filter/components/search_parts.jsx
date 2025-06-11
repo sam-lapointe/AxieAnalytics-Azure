ae0bfc4730c37bf157317f2f7fe0f28e1fe95333
sam-lapointe <samuellapointebusiness@gmail.com>
2025-06-10 20:49:11 -0400

    Continued search_parts.jsx

--- frontend/src/features/axie_filter/components/search_parts.jsx
+++ frontend/src/features/axie_filter/components/search_parts.jsx
@@ -10,10 +10,59 @@ import {
 } from "@/components/ui/popover"
 
 
+const test_parts = {
+    "Anemone": {
+        "class": "Aquatic",
+        "partsIds": [
+            {"id": "anemone", "stage": 1},
+            {"id": "anemone-2", "stage": 2}
+        ]
+    },
+    "Arco": {
+        "class": "Beast",
+        "partsIds": [
+            {"id": "arco", "stage": 1},
+            {"id": "arco-2", "stage": 2}
+        ]
+    },
+    "Croc": {
+        "class": "Reptile",
+        "partsIds": [
+            {"id": "croc", "stage": 1},
+            {"id": "croc-2", "stage": 2}
+        ]
+    },
+    "Nimo": {
+        "class": "Aquatic",
+        "partsIds": [
+            {"id": "nimo", "stage": 1},
+            {"id": "nimo-2", "stage": 2}
+        ]
+    },
+    "Antenna": {
+        "class": "Bug",
+        "partsIds": [
+            {"id": "antenna", "stage": 1},
+            {"id": "antenna-2", "stage": 2}
+        ]
+    }
+}
+
+
 export function SearchParts() {
-    const [parts, setParts] = useState({})
+    const [parts, setParts] = useState(test_parts)
+    const [selectedParts, setSelectedParts] = useState({})
     const [inputValue, setInputValue] = useState("")
     const [isInputFocus, setIsInputFocus] = useState(false)
+
+    const inputRef = useRef(null)
+
+    const handleButtonClick = (axieParts, action) => {
+        if (inputRef.current) {
+            inputRef.current.focus()
+        }
+        console.log(axieParts, action)
+    }
 
     return (
         <div>
@@ -22,21 +71,62 @@ export function SearchParts() {
             <div className="">
                 <Popover open={isInputFocus}>
                     <PopoverAnchor asChild>
-                        <Input 
+                        <Input
+                            ref={inputRef}
                             placeholder="Search parts..."
                             onFocus={() => setIsInputFocus(true)}
-                            onBlur={() => setIsInputFocus(false)}
+                            // onBlur={() => setIsInputFocus(false)}
                             onChange={(e) => {setInputValue(e.target.value)}}
                             className = "focus-visible:ring-0 focus-visible:ring-offset-0 focus:outline-none"
                         />
                     </PopoverAnchor>
                     <PopoverContent
                         onOpenAutoFocus={(e) => e.preventDefault()}
-                        className="w-full min-w-[var(--radix-popover-trigger-width)]"
+                        onPointerDownOutside={(e) => {
+                            const target = e.target
+                            if (inputRef.current && inputRef.current.contains(target)) {
+                                e.preventDefault
+                            } else {
+                                setIsInputFocus(false)
+                            }
+                        }}
+                        className="w-full min-w-[var(--radix-popover-trigger-width)] max-h-90 overflow-y-auto"
                     >
                         <div>
-                            <p>Hello</p>
-                            <p>Hello</p>
+                            {Object.keys(parts).map((part) => {
+                                if (part.toLowerCase().startsWith(inputValue.toLowerCase())) {
+                                    return (
+                                        <div className="m-2 bg-gray-100 p-2 rounded-xl mb-4" key={part}>
+                                            <div className="flex">
+                                                <p className="font-bold">{part}</p>
+                                                <div className="ml-auto gap-2 flex">
+                                                    <Button
+                                                        className="w-14 h-6 text-xs"
+                                                        size="icon"
+                                                        onClick={() => handleButtonClick(parts[part]["partsIds"], "include")}
+                                                    >
+                                                        Include
+                                                    </Button>
+                                                    <Button className="w-14 h-6 text-xs" size="icon">Exclude</Button>
+                                                </div>
+                                            </div>
+                                            {
+                                                parts[part]["partsIds"].map((item) => {
+                                                    return (
+                                                        <div className="flex items-center" key={item.id}>
+                                                            <p className="text-sm">Stage {item.stage}</p>
+                                                                <div className="ml-auto gap-2 flex">
+                                                                <Button className="w-10, h-4" size="icon">+</Button>
+                                                                <Button className="w-10 h-4" size="icon">-</Button>
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                        </div>
+                                    )
+                                }
+                            })}
                         </div>
                     </PopoverContent>
                 </Popover>
